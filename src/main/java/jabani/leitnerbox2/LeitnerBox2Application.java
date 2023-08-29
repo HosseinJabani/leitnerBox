e5a986b56f85b9d8931b44d4f9b36c8efb0b2b80
HosseinJavooni <h.javoooni@gmail.com>
2023-08-29 10:20:52 +0330

    firstCommit

---
+++ src/main/java/jabani/leitnerbox2/LeitnerBox2Application.java
@@ -0,0 +1,13 @@
+package jabani.leitnerbox2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeitnerBox2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeitnerBox2Application.class, args);
+	}
+
+}
---
+++ src/test/java/jabani/leitnerbox2/LeitnerBox2ApplicationTests.java
@@ -0,0 +1,13 @@
+package jabani.leitnerbox2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeitnerBox2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
